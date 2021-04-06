d396c9b8d2fa7f030b64d4ffba1b11246e67e36b
Nina Hochmayr <if19b042@technikum-wien.at>
2021-04-06 17:21:16 +0200

    created DB and added DB Connection (& conf file)

---
+++ TourPlanner.DataAccessLayer/DBConnObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner.DataAccessLayer
+{
+    public class DBConnObject
+    {
+        public string server;
+        public int port;
+        public string user;
+        public string password;
+        public string database;
+    }
+}
--- TourPlanner.DataAccessLayer/DBConnection.cs
+++ TourPlanner.DataAccessLayer/DBConnection.cs
@@ -1,37 +1,56 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using TourPlanner.Models;
 
 namespace TourPlanner.DataAccessLayer
 {
     class DBConnection : IDataAccess
     {
-        private string _connection;
+        private string _connectionString;
+        private NpgsqlConnection _conn;
+        private string _sql;
+        private NpgsqlCommand _cmd;
 
         public DBConnection()
         {
-            //from config file!
-            _connection = loadConfig("blub");
-            //connect to db
+            _connectionString = loadConfig("../../../../database.json");
+            _conn = new NpgsqlConnection(_connectionString);
         }
 
         public string loadConfig(string fileName)
         {
             string connection = null;
-            //read file
+            string jsonString = File.ReadAllText(fileName);
+            DBConnObject conn = JsonConvert.DeserializeObject<DBConnObject>(jsonString);
+
+            Debug.WriteLine($"Server={conn.server};Port={conn.port};User Id={conn.user};Password={conn.password};Database={conn.database};");
+
+            connection = $"Server={conn.server};Port={conn.port};User Id={conn.user};Password={conn.password};Database={conn.database};";
             return connection;
         }
 
         public List<TourItem> GetTours()
         {
-            //select tours statement from db
-            return new List<TourItem>()
-            {
-                new Models.TourItem() { Name = "Kahlenberg", Description = "Wien Nussdorf - Kahlenberg", Distance = 10},
-                new Models.TourItem() { Name = "Cobenzl", Description = "Wien Nussdorf - Cobenzl", Distance = 7},
-                new Models.TourItem() { Name = "Wienerwald", Description = "Wien Neuwaldegg - Rundweg", Distance = 5},
-                new Models.TourItem() { Name = "Prater", Description = "Wien Prater - Rundweg", Distance = 5}
-            };
+            List<TourItem> tours = new List<TourItem>();
+
+            _conn.Open();
+            _sql = "select * from tour";
+            _cmd = new NpgsqlCommand(_sql, _conn);
+            var reader = _cmd.ExecuteReader();
+
+            while (reader.Read())
+                tours.Add(new Models.TourItem { Name = reader.GetString(0), Description = reader.GetString(1), Distance = reader.GetInt32(2) });
+            _conn.Close();
+
+            foreach (TourItem item in tours)
+                Debug.WriteLine(item.Name + item.Description + item.Distance);
+
+            return tours;
+
         }
     }
 }
